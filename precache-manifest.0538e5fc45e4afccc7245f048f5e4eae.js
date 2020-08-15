c4f2621ec7c739065278d5932eb2abb2b7e6b3e1
Toyin Adesina <toyinadesina60@gmail.com>
2020-08-15 11:00:10 +0100

    Updates

---
+++ precache-manifest.0538e5fc45e4afccc7245f048f5e4eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abaad517cb66733c69f26d05f4513c2c",
+    "url": "/gpa_calculator/index.html"
+  },
+  {
+    "revision": "65e36d80c75994f761ae",
+    "url": "/gpa_calculator/static/css/main.b156e9a2.chunk.css"
+  },
+  {
+    "revision": "22f30c80b50251bf9401",
+    "url": "/gpa_calculator/static/js/2.30b43afd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gpa_calculator/static/js/2.30b43afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e36d80c75994f761ae",
+    "url": "/gpa_calculator/static/js/main.e57e301b.chunk.js"
+  },
+  {
+    "revision": "2dcf7b751c49f231e11d",
+    "url": "/gpa_calculator/static/js/runtime-main.1f17b62a.js"
+  }
+]);
